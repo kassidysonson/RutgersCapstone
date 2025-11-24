23b857770a2d381503c79dd8ceafd462b20e14b0
Msuave14 <Matthew_suarez77@hotmail.com>
2025-11-24 15:28:47 -0500

    reverting tutorial page

--- src/components/HowItWorksPage.jsx
+++ src/components/HowItWorksPage.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './HowItWorksPage.css';
 
 const HowItWorksPage = () => {
@@ -19,7 +20,14 @@ const HowItWorksPage = () => {
       <div className="how-it-works-page-container">
         {/* Header Section */}
         <div className="how-it-works-header">
+          <Link to="/" className="back-link">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+              <path d="m15 18-6-6 6-6"/>
+            </svg>
+            Back to Home
+          </Link>
           <h1 className="page-title">How JoinUp Works</h1>
+          
         </div>
 
         {/* Tutorial Video Section */}
