cedc8516361279638c23ada5cdd9f73172cec33d
Msuave14 <Matthew_suarez77@hotmail.com>
2025-11-24 17:05:40 -0500

    fixing other things

--- src/components/ForStudents.jsx
+++ src/components/ForStudents.jsx
@@ -5,9 +5,7 @@ import FindStudents from './FindStudents';
 const ForStudents = () => {
   return (
     <div className="for-students-page">
-      <div className="for-students-header">
-        <h1 className="page-title">For Students</h1>
-      </div>
+      
 
       <div className="find-students-wrapper">
         <FindStudents />
